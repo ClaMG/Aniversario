9d1a2e2e1081dabb5e8eb62956f971f6a91cc724
Cla <157390340+ClaMG@users.noreply.github.com>
2025-03-25 15:06:42 -0300

    vai que né

---
+++ funcao.js
@@ -0,0 +1,6 @@
+function toggleParagrafo() {
+    var paragrafo = document.getElementById("paragrafo");
+
+    // Alterna a classe "show" para controlar a animação
+    paragrafo.classList.toggle("show");
+}
